5e22c8455768cf45aa1612032766b25199687102
Laughing Biscuit <laughingbiscuit@example.com>
2019-01-03 23:33:35 +0000

    altered push script to work for dotfiles too

--- scripts/lbdk-push.sh
+++ scripts/lbdk-push.sh
@@ -2,11 +2,18 @@
 set -e
 set -o pipefail
 
-URL=$(git remote get-url origin)
 USER=$(lpass show -u Github)
 PASS=$(lpass show -p Github)
 
-# run git push and catch error
-ERR=$(git push $(echo $URL | sed "s/https:\/\//https:\/\/$USER:$PASS@/") $@ 2>&1)
+if echo $@ | grep 'dot' -q ; then
+	# use bare repo for dotfiles and always push to master 
+	URL=$(git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote get-url origin)
+	ERR=$(git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME push $(echo $URL | sed "s/https:\/\//https:\/\/$USER:$PASS@/") master 2>&1)
+else
+	# run git push and catch error
+	URL=$(git remote get-url origin)
+	ERR=$(git push $(echo $URL | sed "s/https:\/\//https:\/\/$USER:$PASS@/") $@ 2>&1)
+fi
+
 # obfuscate password
 echo $ERR | sed "s/$PASS/<pass>/"
