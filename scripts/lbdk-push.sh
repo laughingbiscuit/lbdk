9dfd18c9eb39163b013870e133b1bc05c28a9f18
Laughing Biscuit <laughingbiscuit@example.com>
2019-01-03 15:30:40 +0000

    added git push script which adds pass from lpass

---
+++ scripts/lbdk-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+URL=$(git remote get-url origin)
+USER=$(lpass show -u Github)
+PASS=$(lpass show -p Github)
+
+# run git push and catch error
+ERR=$(git push $(echo $URL | sed "s/https:\/\//https:\/\/$USER:$PASS@/") $@ 2>&1)
+# obfuscate password
+echo $ERR | sed "s/$PASS/<pass>/"
