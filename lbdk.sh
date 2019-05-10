81382b8dc41ab982a4c54b73a4783180ecab697b
Laughing Biscuit <sean@laughingbiscuit.com>
2019-05-10 10:05:34 +0000

    ensure idempotency without duplicating gcp repo

--- lbdk.sh
+++ lbdk.sh
@@ -93,7 +93,7 @@ chmod +x $LBDK_DIR/target/php-cs-fixer
 
 # install gcloud sdk
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk kubectl -y
 
