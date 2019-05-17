2a2c8fb7a9b380cbeb86ee5ad59d9ff11512621a
Laughing Biscuit <sean@laughingbiscuit.com>
2019-05-17 16:13:28 +0100

    fixed dotconfig alias

--- scripts/lbdk-games.sh
+++ scripts/lbdk-games.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y nethack-console
 sudo apt-get install -y libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev
 mkdir -p ~/games
 curl -sSL http://www.bay12games.com/dwarves/df_44_12_linux.tar.bz2 | tar -xvj -C ~/games
-dotconfig checkout -- ~/games/df_linux/
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -- ~/games/df_linux/
