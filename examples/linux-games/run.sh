036bc94bce87e005d5668465d1c28f73690ebed4
Laughing Biscuit <sean@laughingbiscuit.com>
2019-08-29 14:11:40 +0100

    fixed df dockerfile

--- examples/linux-games/run.sh
+++ examples/linux-games/run.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 mkdir -p $(pwd)/save
-docker run --privileged -v$(pwd)/save:/root/games/df_linux/data/save/ -it lb/df
+docker build -t lb/df .
+docker run --privileged -v$(pwd)/save:/home/someuser/games/df_linux/data/save -it lb/df
