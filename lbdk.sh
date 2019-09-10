d796c41f05d7e52f273b75dd4f0a6241d3983cd1
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2019-09-10 12:16:15 +0100

    Update lbdk.sh

--- lbdk.sh
+++ lbdk.sh
@@ -3,8 +3,8 @@
 # Laughing Biscuit Development Kit
 #
 #	Description: A scrappy idempotent script to setup my Development Environment
-#	Requirements: Debian Stretch
-LSB_RELEASE="stretch"
+#	Requirements: Debian Buster
+LSB_RELEASE="buster"
 #	Usage: ./lbdk.sh [sudo] [ui] 
 #	Repo: https://github.com/laughingbiscuit/lbdk.git
 #	Author: LaughingBiscuit
